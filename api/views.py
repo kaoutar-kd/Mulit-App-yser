ca6d8c3486b805fe7410a9162360ba0cf0979b22
kaoutar-kd <elkeddadikaoutar@gmail.com>
2024-03-05 14:38:03 +0100

    refactor views

--- api/views.py
+++ api/views.py
@@ -1,23 +1,42 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-from api.utils import check_access, decode_token, encode_token, get_token
+from api.utils import check_access, encode_token
 from .models import Role, User, Image
-from rest_framework import generics,status
+from rest_framework import generics, status
 from .serializers import RoleSerializer, UserSerializer, ImageSerializer
 from rest_framework.exceptions import AuthenticationFailed
-from .serializers import UserSerializer
-import jwt, datetime
+import jwt
+import datetime
+
 
 class RegisterView(generics.GenericAPIView):
     def post(self, request):
+        """
+        Handles user registration.
+
+        Args:
+            request: The HTTP request.
+
+        Returns:
+            Response: A Response object with user data.
+        """
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
+
 class LoginAPIView(generics.GenericAPIView):
     def post(self, request):
+        """
+        Handles user login.
+
+        Args:
+            request: The HTTP request.
+
+        Returns:
+            Response: A Response object with JWT token.
+        """
         username = request.data['username']
         password = request.data['password']
 
@@ -32,29 +51,58 @@ class LoginAPIView(generics.GenericAPIView):
         token = encode_token(user)
 
         response = Response()
-
         response.set_cookie(key='jwt', value=token, httponly=True)
         response.data = {
             'jwt': token
         }
         return Response(response.data, status=status.HTTP_200_OK)
-    
+
+
 class RoleListView(APIView):
     def get(self, request):
+        """
+        Retrieves a list of roles.
+
+        Args:
+            request: The HTTP request.
+
+        Returns:
+            Response: A Response object with role data.
+        """
         roles = Role.objects.all()
         serializer = RoleSerializer(roles, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        """
+        Handles the creation of a new role.
+
+        Args:
+            request: The HTTP request.
+
+        Returns:
+            Response: A Response object with role data or error message.
+        """
         serializer = RoleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class RoleDetailsView(APIView):
 
-    def get(self, request, id = None):
+    def get(self, request, id=None):
+        """
+        Retrieves details of a specific role.
+
+        Args:
+            request: The HTTP request.
+            id: The ID of the role.
+
+        Returns:
+            Response: A Response object with role data or error message.
+        """
         try:
             role = Role.objects.get(role=id)
             serializer = RoleSerializer(role)
@@ -63,6 +111,16 @@ class RoleDetailsView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request, id=None):
+        """
+        Updates details of a specific role.
+
+        Args:
+            request: The HTTP request.
+            id: The ID of the role.
+
+        Returns:
+            Response: A Response object with updated role data or error message.
+        """
         try:
             role = Role.objects.get(role=id)
             serializer = RoleSerializer(role, data=request.data)
@@ -74,6 +132,16 @@ class RoleDetailsView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
     def delete(self, request, id=None):
+        """
+        Deletes a specific role.
+
+        Args:
+            request: The HTTP request.
+            id: The ID of the role.
+
+        Returns:
+            Response: A Response object indicating success or failure.
+        """
         try:
             role = Role.objects.get(role=id)
             operator = role.delete()
@@ -87,12 +155,30 @@ class RoleDetailsView(APIView):
 
 class ImageListView(APIView):
     def get(self, request):
+        """
+        Retrieves a list of images.
+
+        Args:
+            request: The HTTP request.
+
+        Returns:
+            Response: A Response object with image data.
+        """
         check_access(request.headers)
         images = Image.objects.all()
         serializer = ImageSerializer(images, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        """
+        Handles the creation of a new image.
+
+        Args:
+            request: The HTTP request.
+
+        Returns:
+            Response: A Response object indicating success or failure.
+        """
         username = check_access(request.headers)
         user = User.objects.get(username=username)
         if user.role.role != 'beta_player':
@@ -104,9 +190,20 @@ class ImageListView(APIView):
             return Response("Image uploaded successfully", status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class ImageDetailsView(APIView):
 
     def get(self, request, id):
+        """
+        Retrieves details of a specific image.
+
+        Args:
+            request: The HTTP request.
+            id: The ID of the image.
+
+        Returns:
+            Response: A Response object with image data or error message.
+        """
         check_access(request.headers)
         try:
             image = Image.objects.get(id=id)
@@ -116,6 +213,16 @@ class ImageDetailsView(APIView):
             return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request, id):
+        """
+        Updates details of a specific image.
+
+        Args:
+            request: The HTTP request.
+            id: The ID of the image.
+
+        Returns:
+            Response: A Response object with updated image data or error message.
+        """
         username = check_access(request.headers)
         user = User.objects.get(username=username)
         if user.role.role != 'beta_player':
@@ -132,6 +239,16 @@ class ImageDetailsView(APIView):
             return Response("Image does not exist", status=status.HTTP_404_NOT_FOUND)
 
     def delete(self, request, id):
+        """
+        Deletes a specific image.
+
+        Args:
+            request: The HTTP request.
+            id: The ID of the image.
+
+        Returns:
+            Response: A Response object indicating success or failure.
+        """
         username = check_access(request.headers)
         user = User.objects.get(username=username)
         if user.role.role != 'beta_player':
